85819a94a0ed74b331e983bd4aa90aee7e3861a7
Aaron Ellington <aaron@ellington.io>
2023-12-20 20:06:32 -0500

    fix the bug

--- apple/AdBrick/Shared (App)/ViewController.swift
+++ apple/AdBrick/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "org.adbrick.AdBrick.Extension"
+let extensionBundleIdentifier = "org.adbrick.AdBrickV1.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
